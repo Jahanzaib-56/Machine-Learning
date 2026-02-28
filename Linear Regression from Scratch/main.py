de3da0ea0649282b4bcd9f95c2cf22ee7597a9f5
Muhammad Jahanzaib Tariq Hashmi <zaibeehashmi@gmail.com>
2026-02-28 12:56:51 +0500

    Update main.py

--- Linear Regression from Scratch/main.py
+++ Linear Regression from Scratch/main.py
@@ -31,3 +31,27 @@ def compute_gradient_descent(df, initial_b, initial_m, learningRate, num_iterati
         b, m = step_gradient(b, m, np.array(df), learningRate)
 
     return [b, m]
+
+def step_gradient(curr_b, curr_m, df, learningRate):
+
+    # Starting point for our gradient
+    b_gradient = 0
+    m_gradient = 0
+
+    N = float(len(df))
+
+    # Iterating over each point
+    for i in range(0, len(df)):
+
+        X = df[i, 0]
+        Y = df[i, 1]
+
+        # Computing partial derivatives of our error function
+        b_gradient += -(2/N) * (Y - ((curr_m * X) + curr_b))
+        m_gradient += -(2/N) * X * (Y - ((curr_m * X) + curr_b))
+
+    # Updating values of b  and m using partial derivatives.
+    new_b = curr_b - (learningRate * b_gradient)
+    new_m = curr_m - (learningRate * m_gradient)
+
+    return [new_b, new_m]
