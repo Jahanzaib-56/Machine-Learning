0f83dc65e6a57761a8abafc4ee651ef12593711d
Muhammad Jahanzaib Tariq Hashmi <zaibeehashmi@gmail.com>
2026-02-28 12:56:09 +0500

    Update main.py

--- Linear Regression from Scratch/main.py
+++ Linear Regression from Scratch/main.py
@@ -19,3 +19,15 @@ def compute_error(b, m, df):
 
     # Get the Mean Error
     return totalError / float(N)
+
+''' Implementing Gradient Descent '''
+def compute_gradient_descent(df, initial_b, initial_m, learningRate, num_iteration):
+    
+    b = initial_b
+    m = initial_m
+
+    for i in range(num_iteration):
+
+        b, m = step_gradient(b, m, np.array(df), learningRate)
+
+    return [b, m]
